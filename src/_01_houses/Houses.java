2e8c9c981521fd601d10f6c1e352ec8d55a95af8
root <root@2f4f627cb551>
2020-12-08 23:27:29 +0000

    Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,18 @@
 package _01_houses;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
-		
+	//Have a robot start on the left side of the window near the bottom.
+		Robot Bot = new Robot();
+		Bot.moveTo(100, 500);
+	//Draw a flat-topped house of height 100 with grass after it 
+		for (int i = 0; i < 10; i++) {
+			Bot.setPenColor(Color.CYAN);
+			
+		}
 	}
 }
