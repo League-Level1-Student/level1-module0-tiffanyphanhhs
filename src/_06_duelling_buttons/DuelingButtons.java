d0858e4d0877049cc44208c94d8982be20bdb2b8
root <root@f90f2f612717>
2021-04-06 21:56:36 +0000

    fortune cookies

--- src/_06_duelling_buttons/DuelingButtons.java
+++ src/_06_duelling_buttons/DuelingButtons.java
@@ -27,7 +27,7 @@ public class DuelingButtons implements ActionListener {
 	public void run() {
 
 		// 1. Add the panel to the frame
-		JFrame frame = new JFrame();
+		frame.add(panel);
 		// 2. Make the frame visible
 		frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
@@ -35,31 +35,46 @@ public class DuelingButtons implements ActionListener {
 		// 4. Set the text of the rightButton to "Click me!"
 		rightButton.setText("Click me!");
 		// 5. Add an action listener to the leftButton
-
+		leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+		rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+		panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+		panel.add(rightButton);
 		// 9. Pack the frame
-
+		frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
 		frame.setTitle("Demanding Buttons");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
-
+		
 		/* If the buttonPressed was the leftButton.... */
 		// Set the text of the rightButton to "No, click Me!"
 		// Set the PREFERRED size of the rightButton to BIG
 		// Set the text of the leftButton to "Click Me!"
 		// Set the PREFERRED size of the leftButton to SMALL
 
+		if(buttonPressed == leftButton) {
+			rightButton.setText("No,click Me!");
+			rightButton.setPreferredSize(BIG);
+			leftButton.setText("Click Me!");
+			leftButton.setPreferredSize(SMALL);
+		}
+		
 		/* If the buttonPressed was the rightButton, do the opposite. */
-
+		if(buttonPressed == rightButton) {
+			leftButton.setText("No,click Me!");
+			leftButton.setPreferredSize(BIG);
+			rightButton.setText("Click Me!");
+			rightButton.setPreferredSize(SMALL);
+		}
 		frame.pack();
 	}
 }
